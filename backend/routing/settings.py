a477cc1bcd0ec561727e478db1dabc17dab353d0
Ubuntu <ubuntu@instance-1.us-central1-a.c.portfolio-344119.internal>
2022-03-15 23:11:38 +0000

    backend support for images

--- backend/routing/settings.py
+++ backend/routing/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = 'https://34.71.5.104/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+
