43676cf7e6d1d9c7f956bf4121995f9701fad7bd
Ryan Lovallo <rlovallo@umich.edu>
2022-04-07 13:31:41 -0400

    Added search filtering for name

--- PortfolioApp/PortfolioApp/LikedUsersView.swift
+++ PortfolioApp/PortfolioApp/LikedUsersView.swift
@@ -12,59 +12,67 @@ struct LikedUsersView: View {
     @Binding var usersViewType: String
     @ObservedObject var store = ProfileStore.shared
     
-    // hardcoded
+    @State private var searchText = ""
+    
     // var likedUserhardcode: LikedUsers = LikedUsers(name: "Rylo")
     
     var body: some View {
-
         NavigationView {
-            
             List {
-                ForEach(store.profiles, id: \.userID) { rw in
+                ForEach(searchResults, id: \.userID) { rw in
                     LikedUserRow(likedUser: rw)
 //                    Text(rw.username!)
                 }
             }
+            .searchable(text: $searchText)
             .refreshable {
                 store.getLikes(id: "10")    // 10 should be the user of the device's ID
+//                print(store.profiles)
             }
             .onAppear {
                 store.getLikes(id: "10")    // 10 should be the user of the device's ID
             }
 
         }
-        
-            .padding()
-            .navigationTitle("SwiftUI Rylo")
-            .toolbar {
-                ToolbarItemGroup(placement: .bottomBar) {
-                    
-                    ZStack {
-                        Button(action: {
-                            self.usersViewType = "home"
-                        }) {
-                            Text("Home")
-                        }
+        .padding()
+        .navigationTitle("SwiftUI Rylo")
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                
+                ZStack {
+                    Button(action: {
+                        self.usersViewType = "home"
+                    }) {
+                        Text("Home")
                     }
-                    
-                    ZStack {
-                        Button(action: {
-                            self.usersViewType = "likedusers"
-                        }) {
-                            Text("Liked Users")
-                        }
-                    }
-                    
-                    ZStack {
-                        Button(action: {
-                            self.usersViewType = "profile"
-                        }) {
-                            Text("Profile")
-                        }
-                    }
-
                 }
+                
+                ZStack {
+                    Button(action: {
+                        self.usersViewType = "likedusers"
+                    }) {
+                        Text("Liked Users")
+                    }
+                }
+                
+                ZStack {
+                    Button(action: {
+                        self.usersViewType = "profile"
+                    }) {
+                        Text("Profile")
+                    }
+                }
+
             }
+        }
+    }
+    
+    var searchResults: [Profile] {
+        if searchText.isEmpty {
+            return store.profiles
+        } else {
+            return store.profiles.filter { ($0.username!).contains(searchText) }
+        }
     }
 }
 
