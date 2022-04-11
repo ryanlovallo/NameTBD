94d3d8ce9f21fbfb5165bb186f929dace9cce1ec
Ryan Lovallo <rlovallo@umich.edu>
2022-04-11 14:35:46 -0400

    Filtering by industry in liked users view

--- PortfolioApp/PortfolioApp/LikedUserRow.swift
+++ PortfolioApp/PortfolioApp/LikedUserRow.swift
@@ -19,8 +19,10 @@ struct LikedUserRow: View {
             
             NavigationLink(destination: OtherUserView(idnum: likedUser.userID!)) {
                 HStack {
+                    let industry = likedUser.industry
                     if let name = likedUser.username {
                         Text(name).padding(EdgeInsets(top: 8, leading: 0, bottom: 6, trailing: 0))
+                        Text(industry!).font(.system(size: 14.0)).fontWeight(.light).padding(EdgeInsets(top: 8, leading: 2, bottom: 6, trailing: 0))
                     }
                     Spacer()
     
